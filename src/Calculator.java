8db0f6d0144c42e7e4b11307659d87e17e57bbf0
Aastha <aastha.2428cseai1398@kiet.edu>
2025-08-28 09:40:25 +0530

    throw

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,16 +4,23 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
         int x, y, z=0;
         System.out.println("Enter any two numbers: ");
-        x = sc.nextInt();
-        y = sc.nextInt();
         try{
+            x = sc.nextInt();
+            y = sc.nextInt();
             z = x/y;
+            System.out.println("exception not occurs");
         }
-//        catch(ArithmeticException e) {
+        catch(ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        catch(NumberFormatException e) {
+            System.out.println(e.getMessage());
+        }
+//        catch(Exception e) {
 //            System.out.println(e.getMessage());
 //        }
         catch(Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e);
         }
         finally {
             System.out.println(z);
---
+++ src/UserDefinedException.java
@@ -0,0 +1,11 @@
+//Step 2 continued...
+public class UserDefinedException {
+    public void validate(int age)throws InvalidAgeException {
+        if(age<18) {
+            throw new InvalidAgeException("NOt Eligible to vote age less than 18");
+        }
+        else {
+            System.out.println("Eligible to vote");
+        }
+    }
+}
