6118eecf6544c261167581b8af8ef3856a239ee9
Aastha <aastha.2428cseai1398@kiet.edu>
2025-09-15 14:29:05 +0530

    HashMap1

--- src/Unit3/HashMap1.java
+++ src/Unit3/HashMap1.java
@@ -1,6 +1,7 @@
 package Unit3;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMap1 {
     public static void main(String[] args) {
@@ -13,5 +14,9 @@ public class HashMap1 {
         System.out.println(hm.keySet());
         System.out.println(hm.values());
         System.out.println(hm.containsValue(2));
+        for(Map.Entry<String,Integer> hm1: hm.entrySet()) {
+            System.out.println(hm1.getKey());
+            System.out.println(hm1.getValue());
+        }
     }
 }
